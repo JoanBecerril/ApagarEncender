11d99e6a457f751975785af8972d6dc36420dd88
Wilson-Alvarracin <alvarracinalfredo@gmail.com>
2024-03-06 20:53:55 +0100

    Estructura de Base de datos Terminada

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,13 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+// MODELOS PARA LA CREACION DE LOS DATOS
+use App\Models\categorias;
+use App\Models\incidencia;
+use App\Models\sedes;
+use App\Models\subcategorias;
+use App\Models\usuarios;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,8 +19,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // CREACION DE SEDES
+        $sede = new sedes();
+        $sede->id = 1;
+        $sede-> nom_sede = "Barcelona";
+        $sede->save();
 
+        $sede2 = new sedes();
+        $sede2->id = 2;
+        $sede2-> nom_sede = "Berlín";
+        $sede2->save();
+
+        $sede3 = new sedes();
+        $sede3->id = 3;
+        $sede3-> nom_sede = "Montreal";
+        $sede3->save();
+
+        // Creacion de
+        // \App\Models\User::factory(10)->create();
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
